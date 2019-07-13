379d480a13abd5699c2960e1b17e73d4d05c6b69
Spencer Wood <spencer.wood25@gmail.com>
2019-07-13 16:32:34 -0400

    updated Link model

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,14 +1,28 @@
 class LinksController < ApplicationController
+  'require securerandom'
   def index
     @link = Link.all
   end
 
   def new
+    @link = Link.new
   end
 
   def show
   end
 
   def create
+    @link = Link.new(link_params)
+    @link.short_link = @link.generate_short_link
+    if @link.save
+      redirect_to @link
+    else
+      render 'new'
+    end
   end
+
+  private
+    def  link_params
+      params.require(:original_link)
+    end
 end
--- app/models/link.rb
+++ app/models/link.rb
@@ -1,2 +1,10 @@
 class Link < ApplicationRecord
+  before_create :generate_short_link
+
+
+  def generate_short_link
+    SecureRandom.urlsafe_base64(4)
+  end
+
+
 end
