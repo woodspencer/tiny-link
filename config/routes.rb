ef8871e85196993c42172541839ddc6a15268b73
Spencer Wood <spencer.wood25@gmail.com>
2019-07-16 22:46:21 -0400

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  resources :links, only: [:index, :new, :goto, :create, :show]
+  resources :links, only: [:index, :new, :create, :show]
   root 'links#index'
   get ':id', to: 'links#goto'
 end
